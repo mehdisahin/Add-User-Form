01000735df05340e8d5489b0c38dbf06cba7e56b
mehdisahin <sahinmehdi23@gmail.com>
2022-12-21 15:08:35 +0300

    getting data from input fields

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
 
 const AddUser = props => {
+	const[enteredUserName, setEnteredUserName] =	useState('');
+	const[enteredAge, setEnteredAge] =	useState('')
 
 const addUserHandler = (event) => {
 	event.preventDefault();
+	console.log(enteredUserName, enteredAge);
 }
+const useNameChangeHandler = (event) => {
+	setEnteredUserName(event.target.value);
+};
+const useAgeChangeHandler = (event) => {
+	setEnteredAge(event.target.value);
+};
 
 return (	
 <Card className={classes.input}>
 <form onSubmit={addUserHandler}>
 		<label htmlFor='username'>Username</label>
-		<input id= 'username' type= 'text'></input>
+		<input id= 'username' type= 'text' onChange={useNameChangeHandler}></input>
 		<label htmlFor='age'>Age (Years)</label>
-		<input id= 'age' type= 'number'></input>
+		<input id= 'age' type= 'number' onChange={useAgeChangeHandler}></input>
 		<Button type= 'submit'>Add User</Button>
 	</form>
 	</Card>
